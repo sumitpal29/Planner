6a609e622c22d74b2dbae06c4aac919fb1747148
Sumit Pal <sumitwuthyou@gmail.com>
2020-05-07 01:20:45 +0530

    leart CURD ops

--- mongodb.js
+++ mongodb.js
@@ -1,12 +1,12 @@
 // client to initialise connection
-const {MongoClient, ObjectID} = require("mongodb");
+const { MongoClient, ObjectID } = require("mongodb");
 
 // define connection URL and databse we wanted to connect
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "planner";
 
 const id = new ObjectID();
-console.log(new Date(id.getTimestamp()))
+console.log(new Date(id.getTimestamp()));
 
 MongoClient.connect(
   connectionURL,
@@ -22,6 +22,22 @@ MongoClient.connect(
     console.log("Connected Successfully");
     // get database reference
     const db = client.db(databaseName);
+
+    db.collection("tasks")
+      .updateMany(
+        {
+          completed: true,
+        },
+        {
+          $set: {
+            completed: false,
+          },
+        }
+      )
+      .then((result) => {
+        console.log(result.modifiedCount);
+      })
+      .catch((err) => console.log(err));
 
     // db.collection('users').insertMany([{
     //     name: "Pritha Mukherjee",
@@ -50,6 +66,43 @@ MongoClient.connect(
     // ], (err, result) => {
     //     if(err) return console.log("Error in inserting data!!")
     //     console.log(result.ops);
+    // });
+
+    // db.collection('users').findone({
+    //   name: 'Sumit'
+    // }, (err, res) => {
+    //   if(err) {
+    //     console.log(err);
+    //   }
+    //   console.log(res)
+    // })
+    // db.collection("users")
+    //   .find({
+    //     age: 26,
+    //   })
+    //   .toArray((err, res) => {
+    //     if (err) {
+    //       console.log(err);
+    //     }
+    //     console.log(res);
+    //   });
+
+    // db.collection("tasks")
+    //   .find({
+    //     completed: false,
+    //   })
+    //   .count((err, res) => {
+    //     if (err) {
+    //       console.log(err);
+    //     }
+    //     console.log(res);
+    //   })
+
+    // .toArray((err, res) => {
+    //   if (err) {
+    //     console.log(err);
+    //   }
+    //   console.log(res);
     // });
   }
 );
