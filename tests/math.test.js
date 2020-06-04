54e2fe58a4aae05bad7a6de3774355eccaf202d5
Sumit Pal <sumitwuthyou@gmail.com>
2020-06-04 11:55:37 +0530

    setting up test cases

---
+++ tests/math.test.js
@@ -0,0 +1,20 @@
+// Jest provides test function globally across the project 
+// test('string - name for your test', funtion)
+
+// test why
+// testing saves time
+// easy to maintain function/features in a big project
+// creates reusable software
+// In future changes, bugs will easily identified.
+// flexibility
+// - refactoring
+// - collabs
+// - profiling -> speed check 
+
+test('Hello World', () => {
+
+});
+
+test('this should fail', () => {
+    throw new Error('Failed!!!')
+})
